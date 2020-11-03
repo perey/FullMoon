d6e24c02844032193af383cd02e5655441bc0740
Tim Pederick <pederick@gmail.com>
2020-11-03 20:53:51 +0800

    Got some basic graphics to display.

--- src/fullmoon.js
+++ src/fullmoon.js
@@ -1,12 +1,54 @@
-"use strict";
+(function () {
+    "use strict";
 
-var scene = Phaser.Scene("placeholder");
+    // The size in pixels of the game area.
+    const WIDTH = 800;
+    const HEIGHT = 600;
 
-var config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    scene: scene
-};
+    // Phaser Scene functions.
 
-var game = new Phaser.Game(config);
+    /**
+     * Preload the game assets.
+     */
+    function preload() {
+        this.load.image("background", "./assets/night-sky.png");
+        this.load.image("moon", "./assets/moon.png");
+    }
+
+    /**
+     * Create the game state after loading is complete.
+     */
+    function create() {
+        var bg = this.add.sprite(0, 0, "background");
+        bg.setOrigin(0, 0);
+        bg.setDisplaySize(WIDTH, HEIGHT);
+
+        this.moon = this.add.sprite(WIDTH / 2.0, HEIGHT / 2.0, "moon");
+        this.moon.setDisplaySize(100, 100);
+    };
+
+    /**
+     * Update the game state.
+     *
+     * Parameters:
+     *   time - The current time, in milliseconds
+     *   delta - The time in milliseconds since the last frame
+     */
+    function update(time, delta) {
+    }
+
+    // Finally, create the game itself.
+
+    let config = {
+        type: Phaser.AUTO,
+        width: WIDTH,
+        height: HEIGHT,
+        scene: {
+            preload: preload,
+            create: create,
+            update: update
+        }
+    };
+
+    var game = new Phaser.Game(config);
+})();
