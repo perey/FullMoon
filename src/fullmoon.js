3b6854e4f841a1ec359a3921a8bd00dc6d583f62
Tim Pederick <pederick@gmail.com>
2020-11-03 17:35:26 +0800

    Got barebones page working.

---
+++ src/fullmoon.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var scene = Phaser.Scene("placeholder");
+
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: scene
+};
+
+var game = new Phaser.Game(config);
