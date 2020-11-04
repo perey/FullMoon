f072328fb7399df6b50817958968461b8c704506
Tim Pederick <pederick@gmail.com>
2020-11-04 17:06:39 +0800

    Got trees to appear.

--- src/fullmoon.js
+++ src/fullmoon.js
@@ -5,6 +5,53 @@
     const WIDTH = 800;
     const HEIGHT = 600;
 
+    // Player data.
+    var Player = new Phaser.Class({
+        initialize: function Player() {
+            this.rot = 0.0;
+        }
+    })
+
+    var player;
+
+    // Game objects.
+
+    // The moon.
+    var Moon = new Phaser.Class({
+        Extends: Phaser.GameObjects.Image,
+
+        initialize: function Moon(scene, image, speed) {
+            Phaser.GameObjects.Image.call(this, scene, WIDTH / 2.0,
+                                           HEIGHT / 2.0, image);
+            this.setDisplaySize(100, 100);
+            this.setVisible(true);
+        }
+    });
+
+    // An immobile item of scenery.
+    var Scenery = new Phaser.Class({
+        Extends: Phaser.GameObjects.Image,
+
+        initialize: function Scenery(scene, sheet, frame, width, height,
+                                     r, theta) {
+            Phaser.GameObjects.Image.call(this, scene, 0, 0, sheet,
+                                          frame);
+            this.setDisplaySize(width, height);
+            this.r = r;
+            this.theta = theta;
+            this.updatePosition();
+        },
+
+        updatePosition: function () {
+            let x = this.theta;
+            let y = HEIGHT / 2.0;
+            this.setPosition(x, y);
+        }
+    });
+
+    // A collection of Scenery instances.
+    var scenery;
+
     // Phaser Scene functions.
 
     /**
@@ -12,19 +59,44 @@
      */
     function preload() {
         this.load.image("background", "./assets/night-sky.png");
+        this.load.image("ground", "./assets/ground.png");
         this.load.image("moon", "./assets/moon.png");
+        this.load.spritesheet("trees", "./assets/trees.png",
+                              {frameWidth: 150, frameHeight: 200});
     }
 
     /**
      * Create the game state after loading is complete.
      */
     function create() {
-        var bg = this.add.sprite(0, 0, "background");
+        player = new Player();
+
+        let bg = this.add.image(0, 0, "background");
         bg.setOrigin(0, 0);
         bg.setDisplaySize(WIDTH, HEIGHT);
 
-        this.moon = this.add.sprite(WIDTH / 2.0, HEIGHT / 2.0, "moon");
-        this.moon.setDisplaySize(100, 100);
+        let moon = new Moon(this, "moon", 0);
+        this.add.existing(moon);
+
+        let ground = this.add.image(0, HEIGHT / 2.0, "ground");
+        ground.setOrigin(0, 0);
+        ground.setDisplaySize(WIDTH, HEIGHT / 2.0);
+
+        scenery = this.add.group({
+            classType: Scenery,
+            runChildUpdate: true
+        });
+        // Create some random trees.
+        const availableDesigns = this.textures.get("trees").frameTotal;
+        let trees = [];
+        for (let i = 0; i < 3; i++) {
+            let randomDesign = Math.round(availableDesigns * Math.random());
+            let tree = new Scenery(this, "trees", randomDesign,
+                                   150, 200, WIDTH * Math.random(),
+                                   HEIGHT * Math.random());
+            trees.push(tree);
+        }
+        scenery.addMultiple(trees, true);
     };
 
     /**
